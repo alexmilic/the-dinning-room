7e7ebe87819fc6f363de1083a49c20ddfda7ead3
Aleksandar Milic <alexmilic01@gmail.com>
2023-05-13 14:35:14 +0200

    Added assets and some components/views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './views/home/home.component';
+import { RestaurantsComponent } from './views/restaurants/restaurants.component';
+import { BannerComponent } from './core/banner/banner.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RestaurantsComponent,
+    BannerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
